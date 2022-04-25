45fa1ca4756a980862b1df26c70dc56ad37210bf
marcorv6 <yosoymarco3@gmail.com>
2022-04-24 21:58:55 -0500

    include pagos

--- server/controller/Pago/pago.js
+++ server/controller/Pago/pago.js
@@ -2,10 +2,11 @@ const helperPath = '../../helper';
 const {validarId} = require(`${helperPath}/validar`);
 const Pago = require('../../db/tablas/Pago');
 const Plan = require('../../db/tablas/Plan');
+const Trabajador = require('../../db/tablas/Trabajador');
 
 const pago = async (query) => {
   const idPago = validarId(query.idPago)
-  return Pago.findOne({ where: { idPago }, include: [{model: Plan}] }).then(res => {
+  return Pago.findOne({ where: { idPago }, include: [{model: Plan}, {model: Trabajador}] }).then(res => {
     if(!res) throw new Error("No se ha encontrado un cliente con ese id.")
     console.log(res)
     return res.dataValues;
--- server/controller/Pago/pagos.js
+++ server/controller/Pago/pagos.js
@@ -1,6 +1,7 @@
 const Pago = require('../../db/tablas/Pago');
 const Plan = require('../../db/tablas/Plan');
+const Trabajador = require('../../db/tablas/Trabajador');
 
-const pagos = () => Pago.findAll({include: [{model:Plan}]});
+const pagos = () => Pago.findAll({include: [{model:Plan}, {model: Trabajador}]});
 
 module.exports = pagos;
